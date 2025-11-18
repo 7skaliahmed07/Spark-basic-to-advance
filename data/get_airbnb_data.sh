3952e82b8168fe42c78758eae678fef74c7a3ade
Sk Ali Ahmed <skprincecoc786@gmail.com>
2025-11-18 18:11:53 +0100

    spark basics

---
+++ data/get_airbnb_data.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+if [ -f "$0" ]; then
+  script=$0
+else
+  script=$(command -v -- "$0")
+fi
+
+dir=$(dirname -- "$script")
+
+echo "Writing data to $dir"
+
+wget -O "$dir/reviews.csv.gz" https://data.insideairbnb.com/united-kingdom/england/london/2024-09-06/data/reviews.csv.gz
+wget -O "$dir/calendar.csv.gz" https://data.insideairbnb.com/united-kingdom/england/london/2024-09-06/data/calendar.csv.gz
+wget -O "$dir/listings.csv.gz" https://data.insideairbnb.com/united-kingdom/england/london/2024-09-06/data/listings.csv.gz
+
+
+
+# Gz to CSV :
+
+# for f in *.gz; do
+#   gzip -dc "$f" > "${f%.gz}"
+# done
